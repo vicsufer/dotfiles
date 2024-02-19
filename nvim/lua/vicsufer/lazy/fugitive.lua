fbbcf370c348771150969215d19c61fe38b3f84e
Víctor Suárez Fernández <15276132+vicsufer@users.noreply.github.com>
2024-02-19 10:11:26 +0100

    Update fugitive.lua

--- nvim/lua/vicsufer/lazy/fugitive.lua
+++ nvim/lua/vicsufer/lazy/fugitive.lua
@@ -3,11 +3,11 @@ return {
     config = function() 
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-        local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+        local vicsufer_Fugitive = vim.api.nvim_create_augroup("vicsufer_Fugitive", {})
 
         local autocmd = vim.api.nvim_create_autocmd
         autocmd("BufWinEnter", {
-            group = ThePrimeagen_Fugitive,
+            group = vicsufer_Fugitive,
             pattern = "*",
             callback = function()
                 if vim.bo.ft ~= "fugitive" then
