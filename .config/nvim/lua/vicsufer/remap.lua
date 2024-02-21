d1975b6b840ee8c92a296e0f2d84b43b44ea63ff
Víctor Suárez Fernández <vicsufer@gmail.com>
2024-02-21 15:51:16 +0100

    Remap nvim

--- .config/nvim/lua/vicsufer/remap.lua
+++ .config/nvim/lua/vicsufer/remap.lua
@@ -1,4 +1,5 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>w", vim.cmd.w)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
