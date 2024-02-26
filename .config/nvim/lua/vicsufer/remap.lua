89d55144ef7c8b1962a7c43686dff7099ad7d86c
Víctor Suárez Fernández <vicsufer@gmail.com>
2024-02-26 14:14:39 +0100

    Update dotfiles

--- .config/nvim/lua/vicsufer/remap.lua
+++ .config/nvim/lua/vicsufer/remap.lua
@@ -1,5 +1,5 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>w", vim.cmd.w)
+vim.keymap.set("n", "<leader>W", vim.cmd.update)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
