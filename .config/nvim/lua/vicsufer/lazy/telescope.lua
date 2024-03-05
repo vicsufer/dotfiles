8077924b87b94511c993944ce10fe27d1de8f616
Víctor Suárez Fernández <vicsufer@gmail.com>
2024-03-05 21:11:47 +0100

    Create proper shortcuts

--- .config/nvim/lua/vicsufer/lazy/telescope.lua
+++ .config/nvim/lua/vicsufer/lazy/telescope.lua
@@ -8,7 +8,19 @@ return {
     },
 
     config = function()
-        require('telescope').setup({})
+        local actions = require("telescope.actions")
+        require('telescope').setup({
+            defaults = {
+                path_display = { "truncate " },
+                mappings = {
+                    i = {
+                        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+                        ["<C-j>"] = actions.move_selection_next,     -- move to next result
+                        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                    },
+                },
+            },
+        })
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Find file" })
